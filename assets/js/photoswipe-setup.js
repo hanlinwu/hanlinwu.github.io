46ca961d1223ffa7db0f424689e0202a294dab7c
hanlinwu <hlwu.bnu@gmail.com>
2026-03-01 06:16:59 +0000

    Deploying to gh-pages from @ hanlinwu/hanlinwu.github.io@f10ae00d10b699b0b351198853664bb9d121221e 🚀

--- assets/js/photoswipe-setup.js
+++ assets/js/photoswipe-setup.js
@@ -1,5 +1,5 @@
-import PhotoSwipeLightbox from "https://jsd.cdn.zzko.cn/npm/photoswipe@5.4.4/dist/photoswipe-lightbox.esm.min.js";
-import PhotoSwipe from "https://jsd.cdn.zzko.cn/npm/photoswipe@5.4.4/dist/photoswipe.esm.min.js";
+import PhotoSwipeLightbox from "https://cdn.jsdmirror.com/npm/photoswipe@5.4.4/dist/photoswipe-lightbox.esm.min.js";
+import PhotoSwipe from "https://cdn.jsdmirror.com/npm/photoswipe@5.4.4/dist/photoswipe.esm.min.js";
 const photoswipe = new PhotoSwipeLightbox({
   gallery: ".pswp-gallery",
   children: "a",
