4654c105fe8d71b27cecda0c22a217507d902dab
hanlinwu <hlwu.bnu@gmail.com>
2026-06-05 09:16:34 +0000

    Deploying to gh-pages from @ hanlinwu/hanlinwu.github.io@4a82e3b2ede31b56c0c5efc02c0d77fd2910b710 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-cv",
-          title: "CV",
-          description: "Curriculum Vitae of Hanlin Wu.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
         },{id: "nav-funding",
           title: "Funding",
           description: "Research grants and funding.",
